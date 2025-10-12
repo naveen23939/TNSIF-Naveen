e386a6ac2369bffea5618bb67cfbadcb558a1d96
NAVEENKUMAR <NAVEENKUMAR@LAPTOP-4KHPG3MK>
2025-10-12 20:24:20 +0530

    Basic code

---
+++ TNSIF/src/introofjava/Basiccode.java
@@ -0,0 +1,28 @@
+package introofjava;
+
+public class Basiccode {
+
+	public static void main(String[] args) {
+		
+		int age = 20;
+		double salary = 50000.00;
+		boolean isPassed = true;
+		char grade = 'A';
+		
+		
+		double newSalary = age; 
+		int roundSalary = (int)salary;
+		
+		int bonus = 5000;
+		int total = roundSalary + bonus;
+		
+		
+		System.out.println(age);
+		System.out.println(salary);
+		System.out.println(roundSalary);
+		System.out.println(total);
+		
+
+	}
+
+}
